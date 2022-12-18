31413bf7863f74d86764ec4da4ff3c64d80ba8fd
Görkem YAVUZ <85451018+grkmyvz@users.noreply.github.com>
2022-12-18 17:42:02 +0300

    Add files via upload

---
+++ homework_1/counter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint public count;
+
+    function inc() public {
+        count += 1;
+    }
+
+    function dec() public {
+        count -= 1;
+    }
+}
